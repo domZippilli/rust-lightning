dbddbc6ce2c00dfdd5f1524b8d4ea72fbe363c62
Elias Rohrer <dev@tnull.de>
2023-08-01 10:37:11 +0200

    Add `lightning-storage` crate
    We add a new `lightning-storage` crate for `KVStore` implementations
    which will supersede the `lightning-persister` crate.

---
+++ lightning-storage/src/lib.rs
@@ -0,0 +1,10 @@
+//! Provides utilities for LDK data persistence and retrieval.
+//
+// TODO: Prefix these with `rustdoc::` when we update our MSRV to be >= 1.52 to remove warnings.
+#![deny(broken_intra_doc_links)]
+#![deny(private_intra_doc_links)]
+
+#![deny(missing_docs)]
+#![deny(unsafe_code)]
+
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
